b2ee95470dd8c4fe781ba1b776c8116cc9d1ff11
Mungi <will2you@gmail.com>
2018-05-22 23:27:15 +0900

    Update pv-sample.ps1

--- pv-sample.ps1
+++ pv-sample.ps1
@@ -1,4 +1,4 @@
-$ErrorActionPreference = "Stop"
+#$ErrorActionPreference = "Stop"
 
 Set-TimeZone -Id "UTC"
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine -Force
@@ -7,11 +7,13 @@ iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
 choco feature enable -n allowEmptyChecksums
 
 winrm quickconfig -force
-timeout /t 10
-winrm set winrm/config/service/auth @{Basic="true"}
-winrm set winrm/config/service @{AllowUnencrypted="true"}
-winrm set winrm/config/client/auth @{Basic="true"}
-#winrm set winrm/config/client @{TrustedHosts = "172.16.10.1"}
+#Enable-PSRemoting -Force
+Start-Sleep -s 15
+#Set-Item -Force WSMan:\localhost\Client\TrustedHosts -Value "172.16.10.1"
+Set-Item -Force WSMan:\localhost\Service\AllowUnencrypted $True
+Set-Item -Force WSMan:\localhost\Client\AllowUnencrypted $True
+Set-Item -Force WSMan:\localhost\Service\Auth\Basic $True
+Set-Item -Force WSMan:\localhost\Client\Auth\Basic $True
 
 $file = $env:SystemRoot + "\Temp\" + (Get-Date).ToString("MM-dd-yy-hh-mm")
 winrm get winrm/config > $file
