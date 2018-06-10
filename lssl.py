8ac4717b359a49304a4c32a6c610401b6266148d
Mungi <will2you@gmail.com>
2018-06-10 12:45:38 +0900

    Update lssl.py

--- lssl.py
+++ lssl.py
@@ -9,6 +9,21 @@ For more information see the SL API at:
 - https://softlayer-python.readthedocs.org/en/latest/config_file.html
 """
 
+# Copyright (C) 2018  mungi <will2you@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import SoftLayer
 import argparse
 import itertools
