0aea63d6faf680fcb4c7e98b2004afe2b414c4c3
Mungi <will2you@gmail.com>
2018-06-13 22:46:27 +0900

    Update pv-cent.sh

--- pv-cent.sh
+++ pv-cent.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+set -x
+timedatectl set-timezone UTC
+date > /tmp/checkdate
+
 yum -y install epel-release
 yum -y install tcpdump bind-utils mtr iperf3 nmon iftop
 yum -y update
