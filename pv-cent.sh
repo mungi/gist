4dd2ed17b16519636d9f7ac34b7283282a682c8b
Mungi <will2you@gmail.com>
2019-03-18 14:22:44 +0900

    Update pv-cent.sh

--- pv-cent.sh
+++ pv-cent.sh
@@ -4,7 +4,7 @@ timedatectl set-timezone UTC
 date > /tmp/checkdate
 
 yum -y install epel-release
-yum -y install tcpdump bind-utils mtr iperf3 nmon iftop
+yum -y install tcpdump bind-utils mtr iperf3 nmon iftop net-tools
 yum -y update
 
 # to do something
